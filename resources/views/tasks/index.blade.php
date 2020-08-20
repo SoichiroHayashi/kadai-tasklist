1f552c40dfd67b7411a1f79708ff3e7388e723c5
Soichiro.Hayashi <enc.buff@gmail.com>
2020-08-20 22:01:38 +0000

    werite-Web-php

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -26,6 +26,6 @@
     @endif
     
     {{-- メッセージ作成ページへのリンク --}}
-    {!! link_to_route('tasks.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
+    {!! link_to_route('tasks.create', '新規コンテンツの投稿', [], ['class' => 'btn btn-primary']) !!}
     
 @endsection
